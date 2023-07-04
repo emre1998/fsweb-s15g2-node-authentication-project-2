dbe0ddf76e697cb6252da2b467c91df4c2ec14de
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-07-04 16:05:28 +0300

    JWT project is all done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // bu secreti kullanın!
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
 
 const sinirli = (req, res, next) => {
   /*
@@ -16,9 +18,24 @@ const sinirli = (req, res, next) => {
 
     Alt akıştaki middlewarelar için hayatı kolaylaştırmak için kodu çözülmüş tokeni req nesnesine koyun!
   */
-}
+  try {
+    const token = req.headers["authorization"];
+    if (!token) {
+      return next({ status: 401, message: "Token gereklidir" });
+    }
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        return next({ status: 401, message: "token gecersizdir" });
+      }
+      req.decodedToken = decodedToken;
+      next();
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-const sadece = role_name => (req, res, next) => {
+const sadece = (role_name) => (req, res, next) => {
   /*
     
 	Kullanıcı, Authorization headerında, kendi payloadu içinde bu fonksiyona bağımsız değişken olarak iletilen 
@@ -30,10 +47,14 @@ const sadece = role_name => (req, res, next) => {
 
     Tekrar authorize etmekten kaçınmak için kodu çözülmüş tokeni req nesnesinden çekin!
   */
-}
 
-
-const usernameVarmi = (req, res, next) => {
+  if (req.decodedToken && req.decodedToken.role_name === role_name) {
+    next();
+  } else {
+    next({ status: 403, message: "Bu, senin için değil" });
+  }
+};
+const usernameVarmi = async (req, res, next) => {
   /*
     req.body de verilen username veritabanında yoksa
     status: 401
@@ -41,8 +62,21 @@ const usernameVarmi = (req, res, next) => {
       "message": "Geçersiz kriter"
     }
   */
-}
-
+  try {
+    const presentUser = await Users.goreBul({
+      username: req.body.username,
+    });
+    if (!presentUser.length) {
+      //array döneceği için uzunluğu 0 olanı yani boş olanı için yazdık
+      next({ status: 401, message: "Geçersiz kriter" });
+    } else {
+      req.user = presentUser[0];
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const rolAdiGecerlimi = (req, res, next) => {
   /*
@@ -63,11 +97,32 @@ const rolAdiGecerlimi = (req, res, next) => {
       "message": "rol adı 32 karakterden fazla olamaz"
     }
   */
-}
+
+  try {
+    const { role_name } = req.body;
+    if (!role_name || !role_name.trim()) {
+      req.role_name = "student";
+      return next();
+    }
+    if (role_name.trim() === "admin") {
+      return next({ status: 422, message: "Rol adı admin olamaz" });
+    }
+    if (role_name.trim().length > 32) {
+      return next({
+        status: 422,
+        message: "rol adı 32 karakterden fazla olamaz",
+      });
+    }
+    req.role_name = role_name.trim();
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   sinirli,
   usernameVarmi,
   rolAdiGecerlimi,
   sadece,
-}
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const { usernameVarmi, rolAdiGecerlimi } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // bu secret'ı kullanın!
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
 
-router.post("/register", rolAdiGecerlimi, (req, res, next) => {
+router.post("/register", rolAdiGecerlimi, async (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -14,6 +17,17 @@ router.post("/register", rolAdiGecerlimi, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  try{
+    const hash = bcrypt.hashSync(req.body.password,10);
+    const newUser = await Users.ekle({
+      role_name:req.role_name,
+      username:req.body.username,
+      password:hash,
+    });
+    res.status(201).json(newUser);
+  } catch(error){
+    next(error);
+  }
 });
 
 
@@ -36,6 +50,25 @@ router.post("/login", usernameVarmi, (req, res, next) => {
       "role_name": "admin" // giriş yapan kulanıcının role adı
     }
    */
+  try{
+    const {password} = req.body;
+    const passwordControl = bcrypt.compareSync(password,req.user.password);
+    if(passwordControl){
+      const token = jwt.sign({
+        subject:req.user.user_id,
+        username:req.user.username,
+        role_name:req.user.role_name,
+      },
+      JWT_SECRET,
+      {expiresIn:"1d"}
+      );
+      res.status(200).json({message:`${req.user.username} geri geldi`,token:token})
+    }else{
+      next({status:401,message:"Geçersiz kriter",});
+    }
+  }catch(error){
+    next(error);
+  }
 });
 
 module.exports = router;
